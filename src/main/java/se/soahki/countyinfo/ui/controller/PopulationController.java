b824e8f8524001cc70ae2d69d67c4473ae37a00f
Anders Björkland <a.bjorkland@live.se>
2017-08-13 13:07:05 +0200

    Added listing of counties and populations to template.

--- src/main/java/se/soahki/countyinfo/ui/controller/PopulationController.java
+++ src/main/java/se/soahki/countyinfo/ui/controller/PopulationController.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import se.soahki.countyinfo.model.County;
 import se.soahki.countyinfo.model.Population;
 import se.soahki.countyinfo.service.PopulationService;
 import se.soahki.countyinfo.utilities.statistics.Statistics;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class PopulationController {
@@ -21,19 +21,36 @@ public class PopulationController {
     public String getTotalPopulationOfYear(@PathVariable Integer year, Model model) {
         List<Population> populationsOfYear = populationService.findByYear(year);
         List<Integer> populationValues = new ArrayList<>();
+        Map<County, Integer> countyPopulations = new HashMap<>();
+        Map<Integer, County> populationsCounty = new HashMap<>();
         for (Population population : populationsOfYear) {
             populationValues.add(population.getInhabitants());
+            County county = population.getMunicipality().getCounty();
+            if (countyPopulations.get(county) == null) {
+                countyPopulations.put(county, population.getInhabitants());
+            } else {
+                Integer incrementInhabitants = population.getInhabitants() + countyPopulations.get(county);
+                countyPopulations.put(county, incrementInhabitants);
+            }
         }
+        for (County county : countyPopulations.keySet()) {
+            populationsCounty.put(countyPopulations.get(county), county);
+        }
+        List<Integer> sortedPopulationsCounty = new ArrayList<>(populationsCounty.keySet());
+        Collections.sort(sortedPopulationsCounty);
+        Collections.reverse(sortedPopulationsCounty);
 
         Statistics stats = new Statistics(populationValues);
         model.addAttribute("stats", stats);
+        model.addAttribute("counties", countyPopulations);
+        model.addAttribute("populationsMap", populationsCounty);
+        model.addAttribute("populations", sortedPopulationsCounty);
         return "statistik";
     }
 
-    private void statistics(List<Population> populations) {
-        double[] values = new double[populations.size()];
-        for (int i = 0; i < populations.size(); i++) {
-            values[i] = populations.get(i).getInhabitants();
-        }
+    private void topCounties(int TopNOfCounties) {
+        List<County> topCounties = new ArrayList<>();
+
+
     }
 }
